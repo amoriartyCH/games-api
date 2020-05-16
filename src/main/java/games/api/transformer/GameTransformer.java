b149f15b36c2cde8fc64806ff3340f1bd2312c45
amoriartyCH <amoriarty@companieshouse.gov.uk>
2020-05-16 09:18:45 +0100

    Implemented epoch storage and conversion to local date

--- src/main/java/games/api/transformer/GameTransformer.java
+++ src/main/java/games/api/transformer/GameTransformer.java
@@ -1,29 +1,58 @@
 package games.api.transformer;
 
+import games.api.model.entity.CommentEntity;
 import games.api.model.entity.GameEntity;
+import games.api.model.rest.Comment;
 import games.api.model.rest.Game;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
-public class GameTransformer implements GenericTransformer<Game, GameEntity> {
+import java.util.ArrayList;
+import java.util.List;
 
-    @Override
+@Component
+public class GameTransformer {
+
+    @Autowired
+    CommentTransformer commentTransformer;
+
     public GameEntity transform(Game rest) {
 
         GameEntity entity = new GameEntity();
 
         BeanUtils.copyProperties(rest, entity);
 
+        //Transform comments using a dedicated transformer as epoch conversion is needed
+        if(!rest.getComments().isEmpty()) {
+            List<CommentEntity> commentEntityList = new ArrayList<>();
+
+            for (Comment c: rest.getComments()) {
+                commentEntityList.add(commentTransformer.transform(c));
+            }
+
+            entity.setComments(commentEntityList);
+        }
+
         return entity;
     }
 
-    @Override
     public Game transform(GameEntity entity) {
 
         Game rest = new Game();
 
         BeanUtils.copyProperties(entity, rest);
+
+        //Transform comments using a dedicated transformer as epoch conversion is needed
+        if(!entity.getComments().isEmpty()) {
+            List<Comment> commentList = new ArrayList<>();
+
+            for (CommentEntity ce: entity.getComments()) {
+                commentList.add(commentTransformer.transform(ce));
+            }
+
+            rest.setComments(commentList);
+        }
 
         return rest;
     }
